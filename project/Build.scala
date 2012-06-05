2184e9cf1af855b13b085703d181a2f0d0aa6f5a
Bayo Erinle <berinle@gmail.com>
2012-06-05 18:04:38 -0400

    migrated to webjars for library management

--- project/Build.scala
+++ project/Build.scala
@@ -9,11 +9,13 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
-      "postgresql"%"postgresql"%"9.1-901-1.jdbc4"
+      "postgresql"%"postgresql"%"9.1-901-1.jdbc4",
+      "com.github.twitter" % "bootstrap" % "2.0.3"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
+      // Add your own project settings here
+      resolvers += "webjars" at "http://webjars.github.com/m2"      
     )
 
 }
